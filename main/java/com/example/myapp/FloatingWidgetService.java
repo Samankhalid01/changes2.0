97234fb6da6b8620f3629d6de1f7624f0247d9ac
Saman <samankalid0001@gmail.com>
2023-09-02 00:20:46 +0500

    first commit

--- main/java/com/example/myapp/FloatingWidgetService.java
+++ main/java/com/example/myapp/FloatingWidgetService.java
@@ -1,5 +1,9 @@
 package com.example.myapp;
-
+import android.graphics.drawable.GradientDrawable;
+import android.util.Log;
+import android.util.TypedValue;
+import androidx.core.content.ContextCompat;
+import android.widget.Button;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
@@ -11,10 +15,9 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Button;
 import android.widget.Toast;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import android.util.TypedValue;
 
 public class FloatingWidgetService extends Service {
 
@@ -36,7 +39,6 @@ public class FloatingWidgetService extends Service {
         chatHeadView = LayoutInflater.from(this).inflate(R.layout.overlay_layout, null);
 
         final WindowManager.LayoutParams params;
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             params = new WindowManager.LayoutParams(
                     WindowManager.LayoutParams.WRAP_CONTENT,
@@ -53,6 +55,7 @@ public class FloatingWidgetService extends Service {
                     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                     PixelFormat.TRANSLUCENT
             );
+
         }
 
         params.gravity = Gravity.TOP | Gravity.START;
@@ -67,18 +70,29 @@ public class FloatingWidgetService extends Service {
         chatHeadIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Show a simple dialog-like view for user input
+                // Here Hide the logo when clicked
+
+                chatHeadIcon.setVisibility(View.INVISIBLE);
+                // Show the chat view
                 showChatView();
             }
         });
     }
 
     private void showChatView() {
-        final LinearLayout chatView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.chat_view, null);
+        // Create a custom dialog-like view
+        final View chatView = LayoutInflater.from(this).inflate(R.layout.chat_view, null);
         final EditText userInputEditText = chatView.findViewById(R.id.user_input_edit_text);
         Button sendButton = chatView.findViewById(R.id.send_button);
-        Button cancelButton = chatView.findViewById(R.id.cancel_button);
+        Button clearButton = chatView.findViewById(R.id.clear_button);
 
+
+        // Apply custom styling to buttons
+        applyButtonStyle(sendButton);
+        applyButtonStyle(clearButton);
+        applyButtonStyle(clearButton);
+
+        // Handle send button click
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,24 +107,43 @@ public class FloatingWidgetService extends Service {
             }
         });
 
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Remove the chat view
-                windowManager.removeView(chatView);
-            }
-        });
 
-        // Add the chat view to the window manager
+
+
+        // Set up layout parameters for the chat view
         WindowManager.LayoutParams chatViewParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT
         );
+
+        // Set position for the chat view
+        chatViewParams.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        chatViewParams.x = 0;
+        chatViewParams.y = 0;
+
+        // Add the chat view to the window manager
         windowManager.addView(chatView, chatViewParams);
     }
+
+    private void applyButtonStyle(Button button) {
+        // Apply custom styling to buttons here
+        button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        button.setTextColor(ContextCompat.getColor(this, android.R.color.black));
+
+        // Set background color and border radius
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setColor(ContextCompat.getColor(this, android.R.color.white)); // Set background color
+        drawable.setCornerRadius(8); // Set border radius in pixels
+        button.setBackground(drawable);
+
+        // Add padding to the button
+        int paddingDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
+        button.setPadding(paddingDp, paddingDp, paddingDp, paddingDp);
+    }
+
 
     @Override
     public void onDestroy() {
